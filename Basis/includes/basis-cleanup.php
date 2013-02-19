b1fa70b502d0f3d654fa6c922db5cb3a5eebe834
Josiah Spence <spencejosiah@gmail.com>
2013-02-19 12:53:37 -0600

    Removed Stripping of Image Dimensions

--- Basis/includes/basis-cleanup.php
+++ Basis/includes/basis-cleanup.php
@@ -281,15 +281,6 @@ function basis_auto_excerpt_more($more) {
 add_filter('excerpt_length', 'basis_excerpt_length');
 add_filter('excerpt_more', 'basis_auto_excerpt_more');
 
-//Remove width and height attributes from image tags for fluid image sizes / responsive design.
-add_filter( 'post_thumbnail_html', 'remove_thumbnail_dimensions', 10 );
-add_filter( 'image_send_to_editor', 'remove_thumbnail_dimensions', 10 );
-
-function remove_thumbnail_dimensions( $html ) {
-    $html = preg_replace( '/(width|height)=\"\d*\"\s/', "", $html );
-    return $html;
-}
-
 // remove container from menus
 function basis_nav_menu_args($args = '') {
 	$args['container'] = false;
