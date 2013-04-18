6f7051bd0b7dde50579d452f188a63427788c955
Josiah Spence <spencejosiah@gmail.com>
2013-04-17 22:51:35 -0500

    Update JQuery Document Ready in Script.js

--- Basis/js/script.js
+++ Basis/js/script.js
@@ -1,5 +1,14 @@
 /* Author: 
 
 */
-// Basic FitVids Test
+
+jQuery(document).ready(function($) {
+
+    // Inside of this function, $() will work as an alias for jQuery()
+    // and other libraries also using $ will not be accessible under this shortcut
+	
+	// Basic FitVids Test
 	$(".main-content").fitVids();
+	
+});
+
