d98f82e8b8634b16c300dba7fd3bdd7c220d1a10
Josiah Spence <spencejosiah@gmail.com>
2013-07-16 22:41:30 -0500

    Add Open Graph Image tag

--- Basis/header.php
+++ Basis/header.php
@@ -15,6 +15,11 @@
 				<!-- Facebook Open Graph Meta for Single Posts -->
 				<meta property="og:image" content="<?php $thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'thumbnail' ); echo $thumb['0']; ?> "/>
 				<meta property="og:description" content="<?php $excerpt = strip_tags(get_the_excerpt()); echo $excerpt; ?>"/>
+				<?php if ( has_post_thumbnail() ) { 
+					$thumb_id = get_post_thumbnail_id();
+					$thumb_url = wp_get_attachment_image_src($thumb_id,’medium’, true); ?>
+					<meta property="og:image" content="<?php echo $thumb_url[0]; ?>" />
+				<?php } ?>
 			<?php endwhile; endif; ?>
 		<?php } else { ?>
 			<meta name="description" content="<?php bloginfo('description'); ?>" />
@@ -31,6 +36,7 @@
 		<?php endif; ?>
 		<?php if ( file_exists(TEMPLATEPATH .'/apple-touch-icon.png') ) : ?>
 			<link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/apple-touch-icon.png">
+			<meta property="og:image" content="<?php echo get_template_directory_uri(); ?>/apple-touch-icon.png" />
 		<?php endif; ?>
 		<meta name="msapplication-TileColor" content="#ee372e">
 		<?php if ( file_exists(TEMPLATEPATH .'/win8-tile-icon.png') ) : ?>
