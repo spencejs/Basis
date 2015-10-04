09c8997734593e7e1f042ece3dceba748f664403
Josiah Spence <spencejosiah@gmail.com>
2015-10-03 23:27:50 -0500

    Add Conditional Meta Box Options

--- Basis/includes/basis-meta-boxes.php
+++ Basis/includes/basis-meta-boxes.php
@@ -14,7 +14,7 @@ If you prefer to use another method to create your custom fields and meta boxes,
  *
  * You also should read the changelog to know what has been changed before updating.
  *
- * For more information, please visit: 
+ * For more information, please visit:
  * @link http://www.deluxeblogtips.com/2010/04/how-to-create-meta-box-wordpress-post.html
  */
 
@@ -105,7 +105,7 @@ $meta_boxes[] = array(
 					'name' => __( 'Date picker', 'rwmb' ),
 					'id'   => "{$prefix}date",
 					'type' => 'date',
-		
+
 					// jQuery date picker options. See here http://api.jqueryui.com/datepicker
 					'js_options' => array(
 						'appendText'      => __( '(yyyy-mm-dd)', 'rwmb' ),
@@ -141,20 +141,102 @@ $meta_boxes[] = array(
  *
  * @return void
  */
-function basis_register_meta_boxes()
+function rw_register_meta_boxes()
 {
 	global $meta_boxes;
 
 	// Make sure there's no errors when the plugin is deactivated or during upgrade
-	if ( class_exists( 'RW_Meta_Box' ) )
-	{
-		foreach ( $meta_boxes as $meta_box )
-		{
+	if ( class_exists( 'RW_Meta_Box' ) ) {
+		foreach ( $meta_boxes as $meta_box ) {
+			if ( isset( $meta_box['only_on'] ) && ! rw_maybe_include( $meta_box['only_on'] ) ) {
+				continue;
+			}
+
 			new RW_Meta_Box( $meta_box );
 		}
 	}
 }
-// Hook to 'admin_init' to make sure the meta box class is loaded
-//  before (in case using the meta box class in another plugin)
-// This is also helpful for some conditionals like checking page template, categories, etc.
-add_action( 'admin_init', 'basis_register_meta_boxes' );
+
+add_action( 'admin_init', 'rw_register_meta_boxes' );
+
+/**
+ * Check if meta boxes is included
+ *
+ * @return bool
+ */
+function rw_maybe_include( $conditions ) {
+	// Include in back-end only
+	if ( ! defined( 'WP_ADMIN' ) || ! WP_ADMIN ) {
+		return false;
+	}
+
+	// Always include for ajax
+	if ( defined( 'DOING_AJAX' ) && DOING_AJAX ) {
+		return true;
+	}
+
+	if ( isset( $_GET['post'] ) ) {
+		$post_id = $_GET['post'];
+	}
+	elseif ( isset( $_POST['post_ID'] ) ) {
+		$post_id = $_POST['post_ID'];
+	}
+	else {
+		$post_id = false;
+	}
+
+	$post_id = (int) $post_id;
+	$post    = get_post( $post_id );
+
+	foreach ( $conditions as $cond => $v ) {
+		// Catch non-arrays too
+		if ( ! is_array( $v ) ) {
+			$v = array( $v );
+		}
+
+		switch ( $cond ) {
+			case 'id':
+				if ( in_array( $post_id, $v ) ) {
+					return true;
+				}
+			break;
+			case 'parent':
+				$post_parent = $post->post_parent;
+				if ( in_array( $post_parent, $v ) ) {
+					return true;
+				}
+			break;
+			case 'slug':
+				$post_slug = $post->post_name;
+				if ( in_array( $post_slug, $v ) ) {
+					return true;
+				}
+			break;
+			case 'not-slug':
+				$post_slug = $post->post_name;
+				if ( !in_array( $post_slug, $v ) ) {
+					return true;
+				}
+			break;
+			case 'category': //post must be saved or published first
+				$categories = get_the_category( $post->ID );
+				$catslugs = array();
+				foreach ($categories as $category) {
+					array_push($catslugs, $category->slug);
+				}
+				if ( array_intersect( $catslugs, $v ) ) {
+					return true;
+				}
+			break;
+			case 'template':
+				$template = get_post_meta( $post_id, '_wp_page_template', true );
+				if ( in_array( $template, $v ) ) {
+					return true;
+				}
+			break;
+		}
+	}
+
+	// If no condition matched
+	return false;
+}
